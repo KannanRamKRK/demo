68dc0b3c4f75d86e1b8aed881cec2b898015bd3a
KannanRamKRK <krkkannan7519@gmail.com>
2019-08-02 19:35:19 +0530

    a

---
+++ Dashboard/app/src/main/java/com/zoom/dashboard/MainActivity.java
@@ -0,0 +1,21 @@
+package com.zoom.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+}
